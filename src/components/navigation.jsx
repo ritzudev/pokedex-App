2090f9b85f1e57932f9fd9a9a81bb2ee3fcec87e
Jimmy Alex Flores Zamudio <alex.flores@telemovilperu.com>
2023-11-21 16:31:29 -0500

    add 'use client'

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 export function Navigation() {
   const getPoke = async ({ name = "charizard" }) => {
     try {
--- src/components/pokecard.jsx
+++ src/components/pokecard.jsx
@@ -1,3 +1,4 @@
+"use client"
 export function PokeCard({
   name,
   sprite,
